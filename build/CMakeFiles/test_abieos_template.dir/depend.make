25606890fad3eda6951463833ba18aba1214ed97
zhanghan <zh_open@163.com>
2021-03-25 16:05:53 +0800

    build

--- build/CMakeFiles/test_abieos_template.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_abieos_template.
-# This may be replaced when dependencies are built.
--- build/tools/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /abieos/tools
-# Build directory: /abieos/build/tools
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
